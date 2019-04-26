a5a20165f470014e86cf29694a0105b1f436f8b5
djohanaris <djohanaris1990@gmail.com>
2019-04-27 02:37:36 +0700

    gunturiswanto

---
+++ AppCordova/www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id8367088_appcordova","rutnuggan","id8367088_appcordova") or die ("could not connect database");
+?>
